c612fc214092e2f1f62dd1b8b60a3d5aa03c3bd1
raluca-dascalu <raluca.florea1@gmail.com>
2021-01-10 19:57:31 +0200

    messenger

---
+++ cmake-build-debug/CMakeFiles/OfflineMessenger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OfflineMessenger.dir/main.cpp.o"
+  "OfflineMessenger"
+  "OfflineMessenger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OfflineMessenger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
